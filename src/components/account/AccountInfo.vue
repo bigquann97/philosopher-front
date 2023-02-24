196c12b00038e76622a67af7df910007e6b85829
lsy <lssy32@naver.com>
2023-02-24 20:06:51 +0900

    refactor: AccountInfo 수정(#9)
    AccountInfo 수정하였습니다.

--- src/components/account/AccountInfo.vue
+++ src/components/account/AccountInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contents">
+  <div class="user-info-contents">
     <h1>내 정보</h1>
     <img
       :src="user.imageUrl"
@@ -8,15 +8,15 @@
     />
     <div class="user-info-form">
       <ul>
-        <h5>이메일:</h5>
+        <h5 class="user-info-title">이메일:</h5>
         <li class="list-group-item">{{ user.email }}</li>
-        <h5>닉네임:</h5>
+        <h5 class="user-info-title">닉네임:</h5>
         <li class="list-group-item">{{ user.nickname }}</li>
-        <h5>성별:</h5>
+        <h5 class="user-info-title">성별:</h5>
         <li class="list-group-item">{{ user.gender }}</li>
-        <h5>나이:</h5>
+        <h5 class="user-info-title">나이:</h5>
         <li class="list-group-item">{{ user.age }}</li>
-        <h5>내 상태:</h5>
+        <h5 class="user-info-title">내 상태:</h5>
         <li class="list-group-item">{{ user.userStatus }}</li>
       </ul>
     </div>
@@ -46,24 +46,40 @@ export default {
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  padding-left: 0;
-}
-
-li {
+.user-info-contents {
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  justify-content: space-between;
-  padding: 0.5rem 0;
-  border-bottom: 1px solid #ddd;
+  margin: 2rem;
 }
 
-h5 {
-  margin: 0;
-  font-size: 1rem;
+.userProfileImage {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 1rem;
+}
+
+.user-info-form {
+  width: 50%;
+  border: 1px solid #ddd;
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+
+.user-info-title {
+  margin-top: 1rem;
+  margin-bottom: 0.5rem;
   font-weight: bold;
-  width: 120px;
-  text-align: left;
+}
+
+.list-group-item {
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  word-break: break-all;
 }
 </style>
