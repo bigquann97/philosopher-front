b1a89895beb97ac01527fa139599a3d20b27ce36
h7359841 <h7359841@gmail.com>
2023-03-02 17:20:47 +0900

    design: 로그인 페이지 디자인 변경(#23)
    로그인 페이지 디자인 변경

--- src/components/account/Signin.vue
+++ src/components/account/Signin.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="contents">
-    <h1>로그인</h1>
+    <div class="signin_title">
+      <img class="signin_logo" src="@/image/logo.png" alt="logo" />
+      <h2>우리 모두 철학자</h2>
+      <p>나만의 철학적인 질문을 모두와 함께 공유하는 토론의 장</p>
+    </div>
     <div class="form-wrapper form-wrapper-sm">
       <input
         id="email"
@@ -8,7 +12,7 @@
         type="text"
         class="validate"
         v-model="email"
-        placeholder="email@email.com"
+        placeholder="아이디(이메일 형식)를 입력해주세요. email@email.com"
       />
       <input
         id="password"
@@ -16,7 +20,7 @@
         type="password"
         class="validate"
         v-model="password"
-        placeholder="비밀번호"
+        placeholder="비밀번호를 입력해주세요."
       />
 
       <p class="log">{{ logMessage }}</p>
@@ -61,4 +65,63 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.wrapper {
+  width: 60%;
+  margin: 0 auto;
+  padding: 1em;
+}
+
+.signin_title {
+  text-align: center;
+}
+
+.signin_logo {
+  width: 60px;
+  border-radius: 50%;
+  margin-top: 90px;
+}
+
+.signin_title h2 {
+  margin-top: 5px;
+  margin-bottom: 0;
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.signin_title p {
+  font-size: 1rem;
+  margin: 8px 0 0;
+}
+
+.form-wrapper {
+  /* border-top: 1px solid gray; */
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 0px;
+  margin-top: 50px;
+  display: flex;
+  flex-flow: row wrap;
+  box-shadow: none;
+}
+
+legend {
+  font-size: 1rem;
+  margin-bottom: 10px;
+  display: block;
+}
+
+label {
+  font-size: 1rem;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+
+button {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
