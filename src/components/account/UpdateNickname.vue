60ddafe1f2dffba74f93610adf544383d9d67339
lsy <lssy32@naver.com>
2023-03-06 03:15:04 +0900

    refactor: 닉네임 변경 코드 수정(#9)
    닉네임 변경 코드 수정하였습니다.

--- src/components/account/UpdateNickname.vue
+++ src/components/account/UpdateNickname.vue
@@ -2,13 +2,12 @@
   <div>
     <label for="nickname-input">닉네임: </label>
     <input type="text" id="nickname-input" v-model="newNickname" />
-    <button @click="changeNickname">변경</button>
+    <button @click="changeNickname" class="updateBtn">변경</button>
     <div v-if="nicknameChanged">닉네임이 변경되었습니다.</div>
   </div>
 </template>
 <script>
 import { modifyAccountNickname } from '@/api/account';
-
 export default {
   data() {
     return {
@@ -19,9 +18,11 @@ export default {
   methods: {
     async changeNickname() {
       try {
-        let nickname = this.newNickname.toString(); // 닉네임을 string 타입으로 변환
+        let nickname = this.newNickname.toString();
         await modifyAccountNickname(nickname);
         this.nicknameChanged = true;
+        alert('닉네임이 변경되었습니다.');
+        this.$router.push({ name: 'accountInfo' }); // accounts 페이지로 이동
       } catch (error) {
         console.error(error);
       }
@@ -29,4 +30,16 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+
+<style scoped>
+.updateBtn {
+  background-color: blue;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  float: right;
+  margin-top: 10px;
+}
+</style>
