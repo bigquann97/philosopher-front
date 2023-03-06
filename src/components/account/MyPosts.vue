cff484455cbd5383c1f9445a723963b20ebede22
lsy <lssy32@naver.com>
2023-03-06 23:01:29 +0900

    refactor: 내 게시물, 댓글, 내가 추천한 게시물,쓰레드 수정(#9)
    전체적인 코드 수정입니다.

--- src/components/account/MyPosts.vue
+++ src/components/account/MyPosts.vue
@@ -11,7 +11,7 @@
       <div class="collection">
         <router-link
           tag="a"
-          :to="{ name: 'MyPosts', params: { id: one.id }, query: { page: 1 } }"
+          :to="{ name: 'Post', params: { id: one.id }, query: { page: 1 } }"
           class="collection-item row"
           v-for="one in list"
           :key="one.id"
