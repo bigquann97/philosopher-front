a2611e964d48c897b5a9394cfc31608d815e0868
lsy <lssy32@naver.com>
2023-03-08 03:57:24 +0900

    refactor: 내 정보 조회 페이지 수정(#61)
    내 정보 조회 페이지 수정하였습니다.

--- src/components/account/AccountInfo.vue
+++ src/components/account/AccountInfo.vue
@@ -1,78 +1,87 @@
 <template>
-  <div class="user-info-contents">
-    <h3>
-      <b>내 정보</b>
-    </h3>
-    <div class="profile-image-container" @click="openFileInput">
-      <img
-        :src="user.imageUrl"
-        alt="사용자 프로필 이미지"
-        class="userProfileImage"
-      />
-      <input
-        type="file"
-        ref="fileInput"
-        style="display: none"
-        @change="onImageSelected"
-      />
-    </div>
-    <div class="user-info-form">
-      <ul>
-        <h5 class="user-info-title">이메일:</h5>
-        <li class="list-group-item">{{ user.email }}</li>
-        <h5 class="user-info-title">닉네임:</h5>
-        <li class="list-group-item">
-          {{ user.nickname
-          }}<router-link to="/updateNickname" class="list-update-item"
-            >닉네임 변경하기</router-link
-          >
-        </li>
-        <li class="list-group-item">
-          <router-link to="/updatePassword" class="list-update-item"
-            >비밀번호 변경하기</router-link
-          >
-        </li>
-        <h5 class="user-info-title">성별:</h5>
-        <li class="list-group-item">{{ user.gender }}</li>
-        <h5 class="user-info-title">나이:</h5>
-        <li class="list-group-item">{{ user.age }}</li>
-        <h5 class="user-info-title">내 상태:</h5>
-        <li class="list-group-item">{{ user.userStatus }}</li>
-        <h5 class="user-info-title">내가 작성한 컨텐츠</h5>
-        <div class="d-flex flex-wrap">
-          <div class="p-2 flex-grow-1">
-            <router-link to="/myPosts" class="list-group-item"
-              >게시글</router-link
-            >
-          </div>
-          <div class="p-2 flex-grow-1">
-            <router-link to="/myComments" class="list-group-item"
-              >댓글</router-link
-            >
-          </div>
+  <div class="user-info-page">
+    <h3 class="user-info-heading">내 정보</h3>
+    <div class="user-info-container">
+      <div class="user-info-item">
+        <span class="user-info-label">프로필 사진:</span>
+        <div class="user-profile-container" @click="openFileInput">
+          <img
+            :src="user.imageUrl"
+            alt="프로필 사진"
+            class="user-profile-image"
+          />
+          <input
+            type="file"
+            ref="fileInput"
+            style="display: none"
+            @change="onImageSelected"
+          />
+          <span class="edit-profile-text">프로필 사진 변경하기</span>
         </div>
-        <h5 class="user-info-title">내가 추천한 컨텐츠</h5>
-        <div class="d-flex flex-wrap">
-          <div class="p-2 flex-grow-1">
-            <router-link to="/recommendedPosts" class="list-group-item"
-              >게시글</router-link
-            >
-          </div>
-          <div class="p-2 flex-grow-1">
-            <router-link to="/recommendedThreads" class="list-group-item"
-              >쓰레드</router-link
-            >
-          </div>
-          <div class="p-2 flex-grow-1">
-            <router-link to="/recommendedComments" class="list-group-item"
-              >댓글</router-link
-            >
-          </div>
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">이메일:</span>
+        <span class="user-info-value">{{ user.email }}</span>
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">닉네임:</span>
+        <span class="user-info-value">{{ user.nickname }}</span>
+        <router-link to="/updateNickname" class="update-info-button"
+          >변경</router-link
+        >
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">비밀번호:</span>
+        <span class="user-info-value">*********</span>
+        <router-link to="/updatePassword" class="update-info-button"
+          >변경</router-link
+        >
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">성별:</span>
+        <span class="user-info-value">{{ user.gender }}</span>
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">나이:</span>
+        <span class="user-info-value">{{ user.age }}</span>
+      </div>
+      <div class="user-info-item">
+        <span class="user-info-label">내 상태:</span>
+        <span class="user-info-value">{{ user.userStatus }}</span>
+      </div>
+      <div class="user-info-separator"></div>
+      <div class="user-info-item">
+        <span class="user-info-label">내가 작성한 컨텐츠:</span>
+        <div class="user-info-buttons">
+          <router-link to="/myPosts" class="content-type-button"
+            >게시글</router-link
+          >
+          <router-link to="/myComments" class="content-type-button"
+            >댓글</router-link
+          >
         </div>
-        <router-link to="/myNotification" class="list-group-item"
+      </div>
+      <div class="user-info-separator"></div>
+      <div class="user-info-item">
+        <span class="user-info-label">내가 추천한 컨텐츠:</span>
+        <div class="user-info-buttons">
+          <router-link to="/recommendedPosts" class="content-type-button"
+            >게시글</router-link
+          >
+          <router-link to="/recommendedThreads" class="content-type-button"
+            >쓰레드</router-link
+          >
+          <router-link to="/recommendedComments" class="content-type-button"
+            >댓글</router-link
+          >
+        </div>
+      </div>
+      <div class="user-info-separator"></div>
+      <div class="user-info-item">
+        <router-link to="/myNotification" class="notification-link"
           >받은 알람</router-link
         >
-      </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -118,68 +127,133 @@ export default {
 };
 </script>
 <style scoped>
-.user-info-contents {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 2rem;
+.user-info-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-top: 20px;
+  padding: 20px;
 }
 
-.userProfileImage {
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  object-fit: cover;
-}
-
-.btn:hover + .userProfileImage {
-  opacity: 0.7;
-}
-
-.user-info-form {
-  width: 50%;
-  border: 1px solid #ddd;
-  padding: 1rem;
-  border-radius: 0.5rem;
-}
-
-.user-info-form li {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-}
-
-.list-group-item {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-}
-
-.user-info-title {
-  margin-top: 1rem;
-  margin-bottom: 0.5rem;
-  font-weight: bold;
-}
-
-.list-group-item {
-  padding: 0.5rem;
-  margin-bottom: 0.5rem;
-  border: none;
-  border-bottom: 1px solid #ddd;
-  word-break: break-all;
-}
-.list-update-item {
-  font-size: 15px;
-}
-
-.profile-image-container {
+.user-info-item {
+  border: 1px solid #ccc;
+  border-radius: 5px;
   margin-bottom: 10px;
+  padding: 10px;
+}
+
+.user-info-item:last-child {
+  margin-bottom: 0;
+}
+.user-info-page {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 30px;
+  box-sizing: border-box;
+  font-size: 18px;
+  line-height: 1.5;
+  color: #333;
+}
+
+.user-info-heading {
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 30px;
+}
+
+.user-profile-container {
+  position: relative;
+  display: inline-block;
+  margin-bottom: 30px;
   cursor: pointer;
 }
 
-.userProfileImage:hover {
-  opacity: 0.7;
-  transition: all 0.3s ease-in-out;
+.user-profile-image {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+.edit-profile-text {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.7);
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.user-profile-container:hover .edit-profile-text {
+  opacity: 1;
+}
+
+.user-info-label {
+  display: inline-block;
+  font-weight: bold;
+  width: 150px;
+}
+
+.user-info-value {
+  display: inline-block;
+  width: calc(100% - 150px);
+  margin-left: 20px;
+}
+
+.update-info-button {
+  display: inline-block;
+  margin-left: 20px;
+  padding: 5px 10px;
+  border-radius: 3px;
+  background-color: #eee;
+  color: #333;
+  text-decoration: none;
+  transition: background-color 0.3s;
+}
+
+.update-info-button:hover {
+  background-color: #ddd;
+}
+
+.user-info-buttons {
+  display: inline-block;
+  margin-left: 20px;
+}
+
+.content-type-button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  background-color: #fff;
+  color: #333;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-decoration: none;
+  margin-right: 1rem;
+}
+
+.content-type-button:hover {
+  background-color: #eee;
+}
+
+.content-type-button:hover {
+  background-color: #ddd;
+}
+
+.notification-link {
+  display: inline-block;
+  margin-left: 170px;
+  padding: 5px 10px;
+  border-radius: 3px;
+  background-color: #eee;
+  color: #333;
+  text-decoration: none;
+  transition: background-color 0.3s;
+}
+
+.notification-link:hover {
+  background-color: #ddd;
 }
 </style>
